1b36176f8d901649e642b03c854874470d598ab7
Victorf73ab <vicf73@hotmail.fr>
2025-07-18 08:45:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // 🗺️ Initialisation de la carte
-  const map = L.map('map').setView([48.0, 15.0], 6);
+  const map = L.map('map').setView([48.0, 15.0], 5);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
