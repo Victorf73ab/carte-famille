e9c40213338014b8127a909b6a77daf7e2a2b3f3
Victorf73ab <vicf73@hotmail.fr>
2025-07-24 09:46:44 +0200

    nouvel essai2

--- script.js
+++ script.js
@@ -226,7 +226,7 @@ Object.entries(latestLocations).forEach(([name, loc]) => {
   (locationGroups[key] = locationGroups[key] || []).push(name);
 });
 
-   // 6) création et ajout des marqueurs
+ // 6) création et ajout des marqueurs
 Object.entries(locationGroups).forEach(([key, group]) => {
   const [lat, lon] = key.split('_').map(Number);
 
@@ -235,97 +235,60 @@ Object.entries(locationGroups).forEach(([key, group]) => {
     const name = group[0];
     const { ville, info } = latestLocations[name];
     validateImage(photoMap[name]).then(url => {
-              const icon = L.icon({
-            iconUrl: url,
-            iconSize: [50, 50],
-            iconAnchor: [25, 25],
-            popupAnchor: [0, -25]
-          });
-          const m = L.marker([lat, lon], { icon })
-            .bindPopup(`<strong>${name}</strong><br>${ville}<br><em>${info}</em>`);
-          m.addTo(map); oms.addMarker(m); markers.push(m);
-        });
-      } else {
-        // Affichage du groupe avec icône "Groupe"
-        const rawGroupPhoto = photoMap["Groupe"] || 'images/group.jpg';
-        validateImage(rawGroupPhoto).then(url => {
-          const icon = L.icon({
-            iconUrl: url,
-            iconSize: [50, 50],
-            iconAnchor: [25, 25],
-            popupAnchor: [0, -25]
-          });
-
-          const gm = L.marker([lat, lon], { icon });
-          gm.addTo(map); oms.addMarker(gm); markers.push(gm);
-
-          // Affiche les individus en décalé uniquement au premier clic
-          let revealed = false;
-          oms.addListener('click', marker => {
-            if (marker === gm && !revealed) {
-              revealed = true;
-              group.forEach((name, i) => {
-                const ind = latestLocations[name];
-                const infoInd = ind.info || '';
-                validateImage(photoMap[name]).then(url2 => {
-                  const icon2 = L.icon({
-                    iconUrl: url2,
-                    iconSize: [50, 50],
-                    iconAnchor: [25, 25],
-                    popupAnchor: [0, -25]
-                  });
-                  const offset = 0.00005 * (i + 1);
-                  const m2 = L.marker([lat + offset, lon + offset], { icon: icon2 })
-                    .bindPopup(`<strong>${name}</strong><br>${ind.ville}<br><em>${infoInd}</em>`);
-                  m2.addTo(map); oms.addMarker(m2); markers.push(m2);
-                });
-              });
-            }
-          });
-        });
-      }
+      const icon = L.icon({
+        iconUrl: url,
+        iconSize: [50, 50],
+        iconAnchor: [25, 25],
+        popupAnchor: [0, -25]
+      });
+      const m = L.marker([lat, lon], { icon })
+        .bindPopup(`<strong>${name}</strong><br>${ville}<br><em>${info}</em>`);
+      m.addTo(map); oms.addMarker(m); markers.push(m);
     });
-
-   oms.addListener('click', marker => {
-  const url = marker.options.icon?.options.iconUrl || '';
-
-  if (url.includes('group.jpg')) {
-    // Si c’est le marqueur "Groupe", éclater les membres
-    if (!marker._revealedMembers) {
-      marker._revealedMembers = true;
-      const matchedGroup = Object.entries(locationGroups).find(([key]) => {
-        const [lat, lon] = key.split('_').map(Number);
-        return Math.abs(marker.getLatLng().lat - lat) < 0.0001 &&
-               Math.abs(marker.getLatLng().lng - lon) < 0.0001;
+  } else {
+    // Affichage du groupe avec icône "Groupe"
+    const rawGroupPhoto = photoMap["Groupe"] || 'images/group.jpg';
+    validateImage(rawGroupPhoto).then(url => {
+      const icon = L.icon({
+        iconUrl: url,
+        iconSize: [50, 50],
+        iconAnchor: [25, 25],
+        popupAnchor: [0, -25]
       });
 
-      if (matchedGroup) {
-        const [key, group] = matchedGroup;
-        const [lat, lon] = key.split('_').map(Number);
-        group.forEach((name, i) => {
-          const ind = latestLocations[name];
-          const infoInd = ind.info || '';
-          const villeInd = ind.ville || '';
-          validateImage(photoMap[name]).then(url2 => {
-            const icon2 = L.icon({
-              iconUrl: url2,
-              iconSize: [50, 50],
-              iconAnchor: [25, 25],
-              popupAnchor: [0, -25]
+      const gm = L.marker([lat, lon], { icon });
+      gm.addTo(map); oms.addMarker(gm); markers.push(gm);
+
+      // Affiche les individus en décalé uniquement au premier clic
+      gm._revealed = false;
+      oms.addListener('click', marker => {
+        if (marker === gm && !gm._revealed) {
+          gm._revealed = true;
+
+          group.forEach((name, i) => {
+            const ind = latestLocations[name];
+            const infoInd = ind.info || '';
+            validateImage(photoMap[name]).then(url2 => {
+              const icon2 = L.icon({
+                iconUrl: url2,
+                iconSize: [50, 50],
+                iconAnchor: [25, 25],
+                popupAnchor: [0, -25]
+              });
+              const offset = 0.00005 * (i + 1);
+              const m2 = L.marker([lat + offset, lon + offset], { icon: icon2 })
+                .bindPopup(`<strong>${name}</strong><br>${ind.ville}<br><em>${infoInd}</em>`);
+              m2.addTo(map); oms.addMarker(m2); markers.push(m2);
+
+              // Force l’éclatement immédiatement
+              oms.spiderfy(gm.getLatLng());
             });
-            const offset = 0.00005 * (i + 1);
-            const m2 = L.marker([lat + offset, lon + offset], { icon: icon2 })
-              .bindPopup(`<strong>${name}</strong><br>${villeInd}<br><em>${infoInd}</em>`);
-            m2.addTo(map); oms.addMarker(m2); markers.push(m2);
           });
-        });
-      }
-    }
-  } else {
-    // Marqueur individuel classique → ouvrir le popup
-    marker.openPopup();
+        }
+      });
+    });
   }
 });
-
+});
   }
 });
